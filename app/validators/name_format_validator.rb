165b76e9807a33055895d76641e1419934fdc804
MaxzyMVT <tvmaxgamer@gmail.com>
2026-07-04 01:36:18 +0700

    fix(groups): allow spaces in group and contest names

--- app/validators/name_format_validator.rb
+++ app/validators/name_format_validator.rb
@@ -1,7 +1,7 @@
 class NameFormatValidator < ActiveModel::EachValidator
   # validates if name is a machine readable
   def validate_each(record, attribute, value)
-    unless value =~ /\A[a-zA-Z\d\-\_\[\]()]+\z/
+    unless value =~ /\A[a-zA-Z\d\-\_\[\]()\ ]+\z/
       record.errors.add(attribute, :name_format)
     end
   end
